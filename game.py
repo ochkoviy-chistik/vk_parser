b9fdf8b938f9b3c4e4efe05da5fdf203ccfa34ca
ochkoviy-chistik <104710901+ochkoviy-chistik@users.noreply.github.com>
2022-10-09 23:36:48 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,105 +1,90 @@
-import math
-import pygame, sys
-from random import randint
+import requests
+import json
+
+# https://oauth.vk.com/authorize?client_id=51435525&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=friends&response_type=token&v=5.131
 
 class Color:
-    def __init__(self):
-        self.r = randint(0, 255)
-        self.g = randint(0, 255)
-        self.b = randint(0, 255)
+    def __init__(self, string):
+        self.string = string
 
-    def get(self):
-        return (self.r, self.g, self.b)
+    def yellow(self):
+        return '\033[38;2;255;255;0m'+self.string+'\033[0;0m'
 
-size = width, height = 1200, 800
-background_color = 0, 0, 0
+    def green(self):
+        return '\033[38;2;0;255;0m'+self.string+'\033[0;0m'
 
+    def blue(self):
+        return '\033[38;2;0;255;255m'+self.string+'\033[0;0m'
 
-def is_intersect(circle1=None, circle2=None):
-    #print(((circle1.x - circle2.x)**2 + (circle1.y - circle2.y)**2)**0.5)
-    return True if ((circle1.x - circle2.x)**2 + (circle1.y - circle2.y)**2)**0.5 < 2*Smile.radius else False
-
-
-class Smile:
-    radius = 50
-    def __init__(self, screen):
-        self.screen = screen
-        self.x, self.y = randint(self.radius, width - self.radius), randint(self.radius, height - self.radius)
-        self.div = 5
-        self.x_div = self.y_div = self.div
-        self.color = Color()
-
-    def mooving(self):
-        pygame.draw.circle(self.screen, self.color.get(), (self.x, self.y), self.radius, 2)
-        # pygame.draw.circle(self.screen, self.color.get(), (self.x + 45, self.y - 30), 15, 2)
-        # pygame.draw.circle(self.screen, self.color.get(), (self.x - 45, self.y - 30), 15, 2)
-        # pygame.draw.line(self.screen, self.color.get(), (self.x, self.y + 20), (self.x, self.y - 30), 2)
-        # pygame.draw.arc(self.screen, self.color.get(), (self.x - 60, self.y + 10, 120, 50), math.pi, 2 * math.pi, 2)
-
-        self.x += self.x_div
-        self.y += self.y_div
-
-        if self.x > width - self.radius:
-            self.x_div = -1*self.div
-            self.color = Color()
-
-        if self.x < self.radius:
-            self.x_div = self.div
-            self.color = Color()
-
-        if self.y > height - self.radius:
-            self.y_div = -1*self.div
-            self.color = Color()
-
-        if self.y < self.radius:
-            self.y_div = self.div
-            self.color = Color()
-
-def intersection(circle1=None, circle2=None):
-    circle1.x_div, circle2.x_div = circle2.x_div, circle1.x_div
-    circle1.y_div, circle2.y_div = circle2.y_div, circle1.y_div
-
-    if circle1.x_div == circle2.x_div and circle1.y_div == circle2.y_div:
-        circle1.x += 25
-        #circle2.x += 10
-        #circle1.y += 10
+    def red(self):
+        return '\033[38;2;255;0;0m'+self.string+'\033[0;0m'
 
 
+try:
+    with open('vk_token.json') as file:
+        try:
+            access_token = json.load(file)['key']
+        except json.decoder.JSONDecodeError:
+            print('Vk токен не найден!')
+except FileNotFoundError:
+    with open('vk_token.json', 'w') as file:
+        print('Создан файл vk_token.json')
+
+
+def get_users_id(id):
+    url = 'https://api.vk.com/method/{}'.format('users.get')
+    response = requests.get(url, params={'user_ids': id,
+                                         'fields': 'schools,occupation',
+                                         'access_token': access_token, 'v': 5.131}).json()
+    #print(response)
+    return response['response']
 
 def main():
-    pygame.init()
-    screen = pygame.display.set_mode(size)
+    url = 'https://api.vk.com/method/{}'.format('friends.get')
+    user = get_users_id(input())[0]
+    response = requests.get(url, params={'user_id': user['id'],
+                                         'count': 1000, 'access_token': access_token, 'v': 5.131}).json()
+    print('У пользователя {} {} найдено {} друзей'.format(user['first_name'], user['last_name'], response['response']['count']))
+    print('-'*30)
 
-    game_over = False
-    smiles = []
-    n = 16
-    for i in range(n):
-        smiles.append(Smile(screen))
+    friends_list = ','.join(list(map(str, response['response']['items'])))
+    friends_list = get_users_id(friends_list)
 
-    while not game_over:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                game_over = True
+    for i in range(len(friends_list)):
+        for j in range(i, len(friends_list)):
+            if friends_list[i]['last_name'] > friends_list[j]['last_name']:
+                friends_list[i], friends_list[j] = friends_list[j], friends_list[i]
 
-        screen.fill(background_color)
+    count = 1
+    flag = False
 
-        for smile in smiles:
-            smile.mooving()
+    with open('marked.json', 'r') as f:
+        s = json.load(f)
 
-        for i in range(len(smiles)):
-            for j in range(i+1, len(smiles)):
-                if is_intersect(smiles[i], smiles[j]):
-                    intersection(smiles[i], smiles[j])
+    for i in friends_list:
+        schools = []
+        if 'deactivated' not in i.keys():
+            if True:
+                if 'occupation' in i.keys():
+                    schools += [i['occupation']['name'].strip()]
 
-        # pygame.draw.rect(screen, (0, 0, 0), (70, 70, 170, 100), 5)
-        # pygame.draw.circle(screen, (0, 255, 0), (width//2, height//2), 100, 5)
-        # pygame.draw.ellipse(screen, (0, 0, 255), (70, 70, 170, 100), 5)
-        # pygame.draw.arc(screen, (0, 0, 255), (70, 70, 170, 100), 0, math.radians(270), 5)
-        # pygame.draw.line(screen, (0,0,0), (70, 70), (270, 200), 1)
+                if 'schools' in i.keys():
+                    schools += [j['name'].strip() for j in i['schools']]
 
-        pygame.display.flip()
-        pygame.time.wait(10)
-    sys.exit()
+                schools = ', '.join(schools)
+
+                if '1543' in schools if flag else True:
+                    if i['last_name'] in s.keys():
+                        if i['first_name'] == s[i['last_name']]:
+                            marked = Color(i['last_name'] + ' ' + i['first_name']).yellow()
+                        else:
+                            marked = Color(i['last_name'] + ' ' + i['first_name']).blue()
+                    else:
+                        marked = Color(i['last_name'] + ' ' + i['first_name']).string
+
+                    print('{}. [https://vk.com/id{} - {}] {} - [{}] - {}'.format(count, i['id'], Color('Закрытый').red() if i['is_closed'] else Color('Открытый').green(),
+                         marked, schools, i['id']))
+                    count+=1
 
 if __name__ == '__main__':
     main()
